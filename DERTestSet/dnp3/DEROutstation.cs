8c76925dd1f2bf87e0d65421742cabb4f0cdf9db
unknown <dupes@.(none)>
2014-04-14 13:41:24 -0500

    add basic outstation

--- DERTestSet/dnp3/DEROutstation.cs
+++ DERTestSet/dnp3/DEROutstation.cs
@@ -4,9 +4,99 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using DNP3.Adapter;
+using DNP3.Interface;
+
 namespace DERTestSet.dnp3
 {
-    class DEROutstation
+    public class DEROutstation : ICommandHandler, ITimeWriteHandler
     {
+        IOutstation m_outstation;
+
+        public DEROutstation()
+        {
+        }
+
+        /*********************************************************************************************/
+
+        public void setOutstation(IOutstation outstation)
+        {
+            m_outstation = outstation;
+        }
+
+        public CommandStatus Supports(ControlRelayOutputBlock command, uint index)
+        {
+            throw new NotImplementedException();
+        }
+
+        /*********************************************************************************************/
+
+        public CommandStatus Supports(AnalogOutputInt32 command, uint index)
+        {
+            throw new NotImplementedException();
+        }
+
+        /*********************************************************************************************/
+
+        public CommandStatus Supports(AnalogOutputInt16 command, uint index)
+        {
+            throw new NotImplementedException();
+        }
+
+        /*********************************************************************************************/
+
+        public CommandStatus Supports(AnalogOutputFloat32 command, uint index)
+        {
+            throw new NotImplementedException();
+        }
+
+        /*********************************************************************************************/
+
+        public CommandStatus Supports(AnalogOutputDouble64 command, uint index)
+        {
+            throw new NotImplementedException();
+        }
+
+        /*********************************************************************************************/
+
+        public CommandStatus Perform(ControlRelayOutputBlock command, uint index)
+        {
+            throw new NotImplementedException();
+        }
+
+        /*********************************************************************************************/
+
+        public CommandStatus Perform(AnalogOutputInt32 command, uint index)
+        {
+            throw new NotImplementedException();
+        }
+
+        /*********************************************************************************************/
+
+        public CommandStatus Perform(AnalogOutputInt16 command, uint index)
+        {
+            throw new NotImplementedException();
+        }
+
+        /*********************************************************************************************/
+
+        public CommandStatus Perform(AnalogOutputFloat32 command, uint index)
+        {
+            throw new NotImplementedException();
+        }
+
+        /*********************************************************************************************/
+
+        public CommandStatus Perform(AnalogOutputDouble64 command, uint index)
+        {
+            throw new NotImplementedException();
+        }
+
+        /*********************************************************************************************/
+
+        public void WriteAbsoluteTime(long millisecSinceEpoch)
+        {
+            Console.Out.WriteLine("time sync: " + millisecSinceEpoch);
+        }
     }
 }
--- DERTestSet/dnp3/OutstationManager.cs
+++ DERTestSet/dnp3/OutstationManager.cs
@@ -11,11 +11,37 @@ namespace DERTestSet.dnp3
 {
     public class OutstationManager
     {
-        IDNP3Manager m_manager;
+        IDNP3Manager m_manager = DNP3ManagerFactory.CreateManager(Environment.ProcessorCount);
+
+        Dictionary<int, DEROutstation> m_outstations = new Dictionary<int, DEROutstation>();
+        Dictionary<string, int> m_outstationNameToIndex = new Dictionary<string, int>();
+
+        /*********************************************************************************************/
 
         public OutstationManager()
         {
-            m_manager = DNP3ManagerFactory.CreateManager(Environment.ProcessorCount);
+        }
+
+        /*********************************************************************************************/
+
+        public void addOutstation(string name, string ipAddress, ushort port)
+        {
+            IChannel channel = m_manager.AddTCPClient(name, LogFilters.INFO, new TimeSpan(0, 0, 5), new TimeSpan(0, 0, 10), ipAddress, port);
+
+            OutstationStackConfig config = new OutstationStackConfig();
+
+            DEROutstation derOutstation = new DEROutstation();
+
+            IOutstation outstation = channel.AddOutstation(name, derOutstation, derOutstation, config);
+
+            derOutstation.setOutstation(outstation);
+
+            int index = m_outstations.Count;
+
+            m_outstations.Add(index, derOutstation);
+            m_outstationNameToIndex.Add(name, index);
+
+            outstation.Enable();
         }
     }
 }
