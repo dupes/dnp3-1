68705133ee9990faf96d2345e80d20704ff60c58
unknown <dupes@.(none)>
2014-04-14 12:48:06 -0500

    initial checkin of DERTestSet

---
+++ DERTestSet/dnp3/OutstationManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using DNP3.Adapter;
+using DNP3.Interface;
+
+namespace DERTestSet.dnp3
+{
+    public class OutstationManager
+    {
+        IDNP3Manager m_manager;
+
+        public OutstationManager()
+        {
+            m_manager = DNP3ManagerFactory.CreateManager(Environment.ProcessorCount);
+        }
+    }
+}
